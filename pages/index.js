ded8026ef2b31dba450d28c64f58f9e9bd9e2a71
daosyn <daosyn@gmail.com>
2018-08-10 01:13:39 -0500

    style sidebar

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,19 @@
-export default () => <div>test</div>
+export default () =>
+    <div className='sidebar'>
+        test
+        <style jsx>{`
+            .sidebar {
+                float: left;
+                background: #111;
+                width: 200px;
+                height: 500px;
+                text-align: left;
+                box-sizing: border-box;
+                padding: 20px;
+                font-family: Monaco;
+                font-size: 11px;
+                color: #fff;
+            }
+        `}</style>
+    </div>
+
