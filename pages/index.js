fb2ccebc2ddbaf760c9776aca647c63bf9a7c8e4
daosyn <daosyn@gmail.com>
2018-08-11 00:19:13 -0500

    display image and add style

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,25 @@
 export default () =>
-    <div className='sidebar'>
-        test
-        <style jsx>{`
-            .sidebar {
-                float: left;
-                background: #111;
-                width: 200px;
-                height: 500px;
-                text-align: left;
-                box-sizing: border-box;
-                padding: 20px;
-                font-family: Monaco;
-                font-size: 11px;
-                color: #fff;
-            }
-        `}</style>
+    <div>
+        <img className="photo" src='/static/sample.jpg'></img>    
+        <div className='sidebar'>test</div>
+            <style jsx>{`
+                .photo {
+                    width: 300px;
+                    height: 300px;
+                }
+
+                .sidebar {
+                    float: left;
+                    background: #111;
+                    width: 200px;
+                    height: 500px;
+                    text-align: left;
+                    box-sizing: border-box;
+                    padding: 20px;
+                    font-family: Monaco;
+                    font-size: 20px;
+                    color: #fff;
+                }
+            `}</style>
     </div>
 
