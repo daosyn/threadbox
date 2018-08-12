3cefe9aec627f3a553760b229b5d89bd8a099306
daosyn <daosyn@gmail.com>
2018-08-11 19:28:55 -0500

    center image and add sample title/text

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,39 @@
 export default () =>
-    <div>
-        <img className="photo" src='/static/sample.jpg'></img>    
-        <div className='sidebar'>test</div>
-            <style jsx>{`
-                .photo {
-                    width: 300px;
-                    height: 300px;
-                }
+    <div className="container">
+        <div className="sidebar">test</div>
+        <div className="content">
+            <img className="photo" src="/static/sample.jpg"></img>
+            <h3>Streetwear Brand</h3>
+            <p>This is a test. This is a test. Thanks for using PayPal. You can now ship any items. To see all the transaction details, log in to your PayPal account. It may take a few moments for this transaction to appear in your account</p>
+        </div>
+        <style jsx>{`
+            .container {
+                padding: 30px;
+            }
 
-                .sidebar {
-                    float: left;
-                    background: #111;
-                    width: 200px;
-                    height: 500px;
-                    text-align: left;
-                    box-sizing: border-box;
-                    padding: 20px;
-                    font-family: Monaco;
-                    font-size: 20px;
-                    color: #fff;
-                }
-            `}</style>
+            .content {
+                margin: auto;
+                width: 400px;
+                height: 400px;
+            }
+
+            .photo {
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                width: 300px;
+                height: auto;
+            }
+
+            .sidebar {
+                float: left;
+                width: 200px;
+                height: 500px;
+                text-align: left;
+                box-sizing: border-box;
+                padding: 20px;
+                font-size: 20px;
+            }
+        `}</style>
     </div>
 
